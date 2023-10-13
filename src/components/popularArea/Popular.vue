3da6dd3c9efe6b32974f34d484b6bb1fe4313dfe
sonsilent783 <sonsilent783@gmail.com>
2023-10-13 21:01:51 +0500

    changes on agent profile button & and header button and popular searches

--- src/components/popularArea/Popular.vue
+++ src/components/popularArea/Popular.vue
@@ -4,8 +4,8 @@
             <div class="col mb-3">
                 <h5>POPULAR SEARCHES</h5>
                 <ul class="nav flex-column">
-                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary font">Properties for rent in DHA Lahore</a></li>
-                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary font">Properties for sale in DHA Lahore</a></li>
+                    <li class="nav-item mb-2"><RouterLink to="/rentPage/?params=rent_sale_type=rent&cities=quetta" class="nav-link p-0 text-body-secondary font">Properties for rent in DHA Lahore</RouterLink></li>
+                    <li class="nav-item mb-2"><RouterLink to="/buyPage/?params=rent_sale_type=sale" class="nav-link p-0 text-body-secondary font">Properties for sale in DHA Lahore</RouterLink></li>
                     <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary font">Properties for rent in DHA Islamabad</a></li>
                     <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary font">Properties for sale in DHA Karachi</a></li>
                     <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary font">Properties for sale in Bahria Town Karachi</a></li>
@@ -57,6 +57,8 @@
     </div>
 </template>
 <script>
+import { RouterLink } from 'vue-router';
+
 
 </script>
 <style>
