17b002387764a2e50fa6a97305269b5fd318c29b
sonsilent783 <sonsilent783@gmail.com>
2023-10-21 20:20:09 +0500

    chnages on blog

--- src/components/newsBlog/Blog.vue
+++ src/components/newsBlog/Blog.vue
@@ -1,18 +1,14 @@
 <template>
+
 <div class="container detailPage">
     <h2 class="display-6 text-center mb-4">News from the Qarbar blog</h2>
     <div class="table-responsive">
-        <div class="'container col-md-4" v-for="details in blogs" :key="details.id">
-            <ul class="list-group d-flex">
-                <div class="card-body">
-                    <li class="list-group-item d-flex justify-content-start align-items-center">20 May <span class="rounded-pill ms-5"></span>
-                        <RouterLink :to="'/blogsDetailPage/' + details?.id" class="nav-link p-0 text-body-read-more-btn text-decoration-none">
-                            <p class="card-text">{{ details?.title }}.</p>
-                        </RouterLink>
-                    </li>
-                </div>
-            </ul>
-        </div>
+        <ul class="list-group" v-for="details in blogs" :key="details.id">
+            <li class="list-group-item d-flex justify-content-start align-items-center"> 20 May <span class="rounded-pill ms-5"> <RouterLink :to="'/blogsDetailPage/'+ details?.id" class="nav-link p-0 text-body-read-more-btn text-decoration-none">
+                <p class="card-text">{{ details?.title }}.</p>
+            </RouterLink></span></li>
+        </ul>
+        <table class="table text-center"></table>
     </div>
 </div>
 </template>
